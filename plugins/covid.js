090fd7ac316285699e30d1cb7f7547d424b21a64
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-27 12:32:14 +0000

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -1,20 +1,20 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. the country?\n\nexample:\n${usedPrefix + command} india`
+  if (!text) throw `uhm.. the country?\n\nexample:\n${usedPrefix + command} morocco`
   let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/' + (text)))
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (json.confirmed) m.reply(`
-Countries : ${text}
-Confirmed : ${json.confirmed.value}
-Recovered : ${json.recovered.value}
-Deaths : ${json.deaths.value}
-Last Update : ${json.lastUpdate}
-\n\n@Ammu`.trim())
+الدولة : ${text}
+الحالات المؤكدة : ${json.confirmed.value}
+المشكوك بها 😂 : ${json.recovered.value}
+الوفيات : ${json.deaths.value}
+آخر تحديث : ${json.lastUpdate}
+\n\n@الغربي`.trim())
   else throw json
 }
 handler.help = ['covid'].map(v => v + ' <country>')
 handler.tags = ['internet']
-handler.command = /^(corona|covid|covid19)$/i
+handler.command = /^(corona|covid|covid19|كوفيد)$/i
 //susu
 module.exports = handler
