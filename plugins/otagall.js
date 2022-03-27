d5b9f8b2a3f76738cc5d6ef169f3a6e79e701598
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-27 12:41:26 +0000

    Update otagall.js

--- plugins/otagall.js
+++ plugins/otagall.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants }) => {
     let users = participants.map(u => u.jid)
-    m.reply(`${text ? `${text}\n` : ''}┌─〔 Tag All 〕\n` + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
+    m.reply(`${text ? `${text}\n` : ''}┌─〔 *منشن جماعي* 〕\n` + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
         contextInfo: { mentionedJid: users }
     })
 }
 
 handler.help = ['otagall']
 handler.tags = ['owner']
-handler.command = ['otagall']
+handler.command = ['tagall|منشن']
 
 handler.owner = true
 
