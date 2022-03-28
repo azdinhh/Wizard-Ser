4155c957d8b6c6782205889e9831cbe76d778290
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-28 18:29:53 +0000

    Update fishing.js

--- plugins/fishing.js
+++ plugins/fishing.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn }) => {
 
   const delay = time => new Promise(res=>setTimeout(res,time));
 
-  if (typeof conn.mancing[m.sender] != "undefined" && conn.mancing[m.sender] == true) return m.reply(`*Can't fish anymore because you are waiting for the fish to catch bro.*`)
+  if (typeof conn.mancing[m.sender] != "undefined" && conn.mancing[m.sender] == true) return m.reply(`*لا يمكن السمك بعد الآن لأنك تنتظر السمكة للقبض على إخوانه*`)
 
   conn.mancing[m.sender] = true
 
   let wait = getRandom(1,5)
   let minute = wait * 1000 * 60
-  m.reply("*Was fishing for " + wait + " minute*\n\n*Please copy and be patient first*")
+  m.reply("*كان يصطاد " + wait + " اللحظة*\n\n*يرجى نسخ والتحلي بالصبر أولا*")
   await delay(minute)
 
   let ikan = ['🐠','🐟','🐡','🐬','🐳','🐋','🦈','🦀','🐊']
@@ -21,39 +21,39 @@ let handler = async (m, { conn }) => {
     if (randIkan2 == "🐠"){
       var jumlahIkan = getRandom(1,25)
       var hargaIkan = 10000
-      var namaIkan = "Clown Fish"
+      var namaIkan = "سمكة المهرج"
     }else if (randIkan2 == "🐟"){
       var jumlahIkan = getRandom(1,13)
       var hargaIkan = 20000
-      var namaIkan = "Blue Fish"
+      var namaIkan = "السمك الأزرق"
     }else if (randIkan2 == "🐡"){
       var jumlahIkan = getRandom(1,7)
       var hargaIkan = 40000
-      var namaIkan = "Puffer fish"
+      var namaIkan = "سمكة منتفخة"
     }else if (randIkan2 == "🐬"){
       var jumlahIkan = getRandom(1,3)
       var hargaIkan = 100000
-      var namaIkan = "Dolphins"
+      var namaIkan = "الدلافين"
     }else if (randIkan2 == "🐳"){		// < 5jt
       var jumlahIkan = getRandom(1,4)
       var hargaIkan = 70000
-      var namaIkan = "Whale"
+      var namaIkan = "حوت"
     }else if (randIkan2 == '🐋'){
       var jumlahIkan = getRandom(1,3)
       var hargaIkan = 100000
-      var namaIkan = "Blue Whale"
+      var namaIkan = "الحوت الأزرق"
     }else if (randIkan2 == "🦈"){
       var jumlahIkan = getRandom(1,5)
       var hargaIkan = 50000
-      var namaIkan = "Shark"
+      var namaIkan = "سمك القرش"
     }else if (randIkan2 == "🐊"){
       var jumlahIkan = getRandom(1,3)
       var hargaIkan = 100000
-      var namaIkan = "Crocodile"
+      var namaIkan = "تمساح"
     }else if (randIkan2 == "🦀"){
       var jumlahIkan = getRandom(1,17)
       var hargaIkan = 15000
-      var namaIkan = "Crab"
+      var namaIkan = "سلطعون"
     }
     
     global.DATABASE._data.users[m.sender].money += hargaIkan * jumlahIkan
