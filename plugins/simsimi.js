652cb50a52ed17a98a38ac1a06f6e08061664a86
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-29 11:19:08 +0000

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. where is the text?\n\nexample:\n${usedPrefix + command} hai`
+  if (!text) throw `اوه.. اين ال text?\n\nexample:\n${usedPrefix + command} هاي`
   let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(text) }, 'apikey'))
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
-  if (json.result == 'I dont understand what you are saying. Please teach me.') await m.reply('The sim hasnt been taught yet, so I dont know t_t the custom message is on , Tell My Master To Teach Me..')
+  if (json.result == 'أنا لا افهم ما تقول. ارجوك علمني.') await m.reply('لم يتم تدريس sim حتى الآن ، لذلك لا أعرف أن الرسالة المخصصة قيد التشغيل ، أخبر سيدي أن يعلمني..')
   await m.reply(`*Simi:* ${json.result}`)
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
