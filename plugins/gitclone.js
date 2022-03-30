7addc797188ed21a71ac1126107acf973e60dc43
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-30 20:30:19 +0000

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -2,7 +2,7 @@ let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let fetch = require('node-fetch')
 let handler = async (m, { args, usedPrefix, command }) => {
 
-    if (!args[0]) throw 'where is the github link? example: https://github.com/DEVILSER/Ammu'
+    if (!args[0]) throw 'اين ال github رابط? مثال: https://github.com/azdinhh/Wizard-Ser/edit/master/plugins/gitclone.js'
 
     if (!regex.test(args[0])) throw 'wrong link!'
 
@@ -11,7 +11,7 @@ let handler = async (m, { args, usedPrefix, command }) => {
     let url = `https://api.github.com/repos/${user}/${repo}/zipball`
     let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
     // 'attachment; filename=Nurutomo-wabot-aq-v2.5.1-251-g836cccd.zip'
-    m.reply(`*Please wait..*`)
+    m.reply(`*أرجو الإنتظار..*`)
     conn.sendFile(m.chat, url, filename, null, m)
 
 }
