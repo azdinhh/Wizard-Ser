d9dca6f741b8103b4520b89c843859b357573503
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-30 20:42:48 +0000

    Update charainfo.js

--- plugins/charainfo.js
+++ plugins/charainfo.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan query!`
+  if (!text) throw `Masukkan استعلام !`
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/character', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   let { name, alternative_names, url, image_url } = json.results[0]
-let charaingfo = `*Name:* ${name}
-*Nickname:* ${alternative_names}
-*Link*: ${url}`
+let charaingfo = `*اسم:* ${name}
+*اسم الشهرة:* ${alternative_names}
+*رابط*: ${url}`
   conn.sendFile(m.chat, image_url, '', charaingfo, m)
 }
 handler.help = ['character <nama>']
