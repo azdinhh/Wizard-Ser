d569322fc47cdbc2571afc0b61d5e64a65bc9848
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-30 21:32:28 +0000

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, text, isROwner, isOwner, usedPrefix, command }) 
     if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply('Welcome set successfully\n@user (Mention)\n@subject (Title Grup)\n@desc (Description Grup)')
-  } else throw `uhm.. where's the text?\n\nExample:\n${usedPrefix + command} welcome @user group @subject\n\n@desc`
+    m.reply('مجموعة الترحيب بنجاح\n@user (Mention)\n@موضوعات (عنوان جروب)\n@desc (وصف جروب)')
+  } else throw `آه .. أين النص?\n\nمثال:\n${usedPrefix + command} welcome @user مجموعة @subject\n\n@desc`
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['owner', 'group']
