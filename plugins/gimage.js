bd5805445889a7d03f3994a87dcd8b9d9ccada81
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-30 19:12:37 +0000

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -4,10 +4,10 @@ let gis = promisify(_gis)
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, command, usedPrefix }) => {
-  if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} Earth`
+  if (!text) throw `اه .. ما الذي تبحث عنه?\n\مثال:\n${usedPrefix + command} أرض`
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
-  if (!url) throw '404 Not Found'
+  if (!url) throw '404 غير موجود'
   conn.sendFile(m.chat, url, 'img', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
 handler.help = ['gimage <search>', 'image <search>','img <search>']
