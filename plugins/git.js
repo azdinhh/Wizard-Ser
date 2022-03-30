7f1fdc536f733e75f7be6673e6ebfd74a0763103
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-30 13:53:05 +0000

    Create git.js

--- plugins/git.js
+++ plugins/git.js
@@ -2,7 +2,14 @@ let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
     let devil = `ㅤ
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/DEVILSER/DEVILSER/main/Media/Ammu/reduced_IMG-20211219-WA0031_2.jpg")).buffer(), devil, 'ᴡɪᴢᴀʀᴅ sᴇʀ  ʙʏ ᴀᴊᴍᴀʟ ᴀɴᴅ ᴀᴄʜᴜ\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ https://github.com/Ajmal-Achu/Wizard-Ser \n\n\ © ī.am ꪶᴡɪᴢᴀʀᴅ sᴇʀꫂ⁩⁴⁰⁴⁩' , '❤️', '.❤️', '🙂', ',🙂')
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://user-images.githubusercontent.com/102433984/160849012-92c670c0-2619-4351-9b07-7683d46dc637.jpg")).buffer(), devil, 'الــغـــࢪبــي\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ 1. *إذا واجهتك مشاكل قلي*
+
+*https://wa.me/+212641427490*
+
+2. *عندك نصائح للتطوير البوت اهلا و سهلا*
+
+3.*رابط مجموعة يتم إضافة كل شهر بوتات جديدة مرحبا بيك*
+*https://chat.whatsapp.com/CZjhxsXctdS1ItNxc60H2d* \n\n\ ©  ꪶالــغـــࢪبــيꫂ⁩⁴⁰⁴⁩' , '❤️', '.❤️', '🙂', ',🙂')
 }
 handler.tags = ['main']
 handler.help = ['git']
