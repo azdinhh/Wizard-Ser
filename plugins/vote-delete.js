2a9d8bb3f5b77c9d452060abbb13afffb3ca8112
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-31 21:17:51 +0000

    Update vote-delete.js

--- plugins/vote-delete.js
+++ plugins/vote-delete.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
     if (!(id in conn.vote)) {
-        await conn.sendButton(m.chat, `_*no voting in this group!*_`, '© Ammu', 'START VOTE', `${usedPrefix}startvote`)
+        await conn.sendButton(m.chat, `_*لا يوجد تصويت في هذه المجموعة!*_`, '© Ammu', 'ابدأ التصويت', `${usedPrefix}startvote`)
         throw false
     }
     delete conn.vote[id]
