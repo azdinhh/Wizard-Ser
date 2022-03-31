e9f37da3df91843606ffef7383794e0456a53bf7
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-03-31 21:23:15 +0000

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
-        await conn.sendButton(m.chat, '_There are still votes in this chat!_', '© Ammu', 'حذف التصويت', `${usedPrefix}حذف الحضور`)
+        await conn.sendButton(m.chat, '_لا تزال هناك أصوات في هذه الدردشة!_', '© الــغـــࢪبــي', 'حذف التصويت', `${usedPrefix}حذف الحضور`)
         throw false
     }
-    await conn.send2Button(m.chat, `Voting starts!
+    await conn.send2Button(m.chat, `يبدأ التصويت!
 *${usedPrefix}تصويت مؤيِّد* - بالتأكيد
 *${usedPrefix}تكريس* - لا
 *${usedPrefix}التصويت* - للتحقق من التصويت
