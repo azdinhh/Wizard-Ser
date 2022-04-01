1870fba633a9415b1f4fff105b290eab9d4347ed
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-01 16:38:23 +0000

    Update link.js

--- plugins/link.js
+++ plugins/link.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
   if (/^[0-9]{5,16}-[0-9]+@g\.us$/.test(args[0])) group = args[0]
   if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'لا يمكن فتحه إلا في مجموعات'
   let groupMetadata = await conn.groupMetadata(group)
-  if (!groupMetadata) throw 'groupMetadata is undefined'
+  if (!groupMetadata) throw 'الكروب البيانات الوصفية غير محدد'
   if (!'participants' in groupMetadata) throw 'لم يتم تعريف المشاركين'
   let me = groupMetadata.participants.find(user => user.jid === conn.user.jid)
   if (!me) throw 'أنا لست في تلك المجموعة :/'
