8f488f3ff67aa4b1fef1d09d84268e8628fda3ca
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-01 20:19:23 +0000

    Update spotify.js

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -6,11 +6,11 @@ let handler = async(m, { conn, text }) => {
   let json = await res.json()
   if(!json.data[0]) throw json
   let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
-let spotifyinfo = `✨️ *Title:* ${title}
-🗣️ *Artists:* ${artists}
-🎆️ *Album:* ${album}
+let spotifyinfo = `✨️ *لقب:* ${title}
+🗣️ *الفنانين:* ${artists}
+🎆️ *البوم:* ${album}
 🌐️ *URL*: ${url}
-💚️ *Direct URL:* ${preview_mp3}\n\n@Ammu`
+💚️ *مباشر URL:* ${preview_mp3}\n\n@Ammu`
 
   await conn.sendFile(m.chat, thumb, '', spotifyinfo, m)
   await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyinfo, m)
