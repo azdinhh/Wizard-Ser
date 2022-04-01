6a4beadd16ecc0caec8379d5fb06a374b266e486
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-01 20:17:33 +0000

    Update lockCmdWithMedia.js

--- plugins/lockCmdWithMedia.js
+++ plugins/lockCmdWithMedia.js
@@ -1,11 +1,11 @@
 module.exports = Object.assign(async function handler(m, { command }) {
-    if (!m.quoted) throw 'reply to the message!'
-    if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
+    if (!m.quoted) throw 'الرد على الرسالة!'
+    if (!m.quoted.fileSha256) throw 'SHA256 الهاش مفقود'
     let sticker = global.db.data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
-    if (!(hash in sticker)) throw 'Hash not found in database'
+    if (!(hash in sticker)) throw 'لم يتم العثور على الهاش في قاعدة البيانات'
     sticker[hash].locked = !/^un/i.test(command)
-    m.reply('Succeed!')
+    m.reply('ينجح!')
 }, {
     rowner: true,
     help: ['un', ''].map(v => v + 'lockcmd'),
