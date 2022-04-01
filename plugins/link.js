852e0c0689c37e00e4d7ff74ea9c4077aed0fa0b
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-01 16:36:46 +0000

    Update link.js

--- plugins/link.js
+++ plugins/link.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, args }) => {
   let group = args[0] ? args[0] : m.chat
   if (/^[0-9]{5,16}-[0-9]+@g\.us$/.test(args[0])) group = args[0]
-  if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'Can only be opened in groups'
+  if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'لا يمكن فتحه إلا في مجموعات'
   let groupMetadata = await conn.groupMetadata(group)
   if (!groupMetadata) throw 'groupMetadata is undefined'
-  if (!'participants' in groupMetadata) throw 'participants is not defined'
+  if (!'participants' in groupMetadata) throw 'لم يتم تعريف المشاركين'
   let me = groupMetadata.participants.find(user => user.jid === conn.user.jid)
   if (!me) throw 'Im not in that group :/'
-  if (me.isAdmin !== true) throw 'Im not an admin '
+  if (me.isAdmin !== true) throw 'أنا لست مشرفًا '
   m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
 }
 handler.help = ['linkgroup']
