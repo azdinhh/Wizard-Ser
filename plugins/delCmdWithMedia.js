26c0b4e2f68442ff40b47f4d85fe5596a3306e21
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-01 19:17:25 +0000

    Update delCmdWithMedia.js

--- plugins/delCmdWithMedia.js
+++ plugins/delCmdWithMedia.js
@@ -1,7 +1,7 @@
 module.exports = Object.assign(async function handler(m, { text }) {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `No hashes`
+    if (!hash) throw `لا تجزئات`
     let sticker = global.db.data.sticker
     if (sticker[hash] && sticker[hash].locked) throw 'ليس لديك إذن بحذف أمر الملصق هذا'
     delete sticker[hash]
