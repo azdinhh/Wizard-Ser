88f5f8fd01101a0f51a01168e7d4ebcd8e18aa60
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-02 13:18:45 +0000

    Update stickerLine.js

--- plugins/stickerLine.js
+++ plugins/stickerLine.js
@@ -4,7 +4,7 @@ const { sticker } = require('../lib/sticker')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-    if (!args[0]) throw `أوم .. أين عنوان url?\n\nexample:\n${usedPrefix + command} https://store.line.me/stickershop/product/`
+    if (!args[0]) throw `أوم .. أين عنوان url?\n\nمثال:\n${usedPrefix + command} https://store.line.me/stickershop/product/`
     if (!args[0].match(/(https:\/\/store.line.me\/stickershop\/product\/.*)/gi)) throw `الــغـــࢪبــي`
 
     let res = await fetch(global.API('zeks', '/api/linesticker', { link: args[0] }, 'apikey'))
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!json.status) throw json
     let hasil = json.sticker.map((v, i) => `${i + 1}. ${v}`).join('\n')
     m.reply(`*${json.title}*
-*Estimated complete:* ${json.sticker.length * 1.5} second
+*يقدر الاكتمال:* ${json.sticker.length * 1.5} ثانيا
     `.trim())
 
     for (let i of json.sticker) {
