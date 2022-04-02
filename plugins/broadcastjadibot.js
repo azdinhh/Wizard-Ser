d1389a7d730f521ee5e9e039d059641bf33acb2c
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-02 11:33:50 +0000

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, text }) => {
   if (conn.user.jid !== global.conn.user.jid) throw false
-  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
+  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'قريب').map(conn => conn.user.jid)])]
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
   let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '*〔 Ammu إذاعة 〕*\n\n' + teks)
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix, text }) => {
     await conn.copyNForward(id, content, true)
   }
   conn.reply(m.chat, `_تم إرسال البث بنجاح إلى ${users.length} الرقم المراد بوت_
-${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${encodeURIComponent(usedPrefix)}menu`).join('\n')}
+${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?نص=${encodeURIComponent(usedPrefix)}menu`).join('\n')}
 \nالتقدير الكامل ${users.length * 1.5} ثانيا`.trim(), m)
 }
 handler.help = ['broadcastjadibot', 'bcbot'].map(v => v + ' <teks>')
