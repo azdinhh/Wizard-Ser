ccc921e7181e39024795ae184b0de29a66d715e5
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-02 12:24:35 +0000

    Update getcode.js

--- plugins/getcode.js
+++ plugins/getcode.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix }) => {
-  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'This command is only for bots', m)
+  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'هذا الأمر مخصص للروبوتات فقط', m)
   else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
 }
 handler.help = ['getcode']
