ce96e659d6a710a502accbb84269e57230fcca21
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-02 17:06:37 +0000

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `إذا وجدت رسالة خطأ, أبلغ عن ذلك باستخدام هذا الأمر\n\nمثال:\n${usedPrefix + command} كيف حالك مطور, لقد وجدت خطأ مثل التالي <copy/ضع علامة على رسالة الخطأ>`
+    if (!text) throw `إذا وجدت رسالة خطأ, أبلغ عن ذلك باستخدام هذا الأمر\n\nمثال:\n${usedPrefix + command} كيف حالك مطور, لقد وجدت خطأ مثل التالي <ينسخ/ضع علامة على رسالة الخطأ>`
     if (text.length < 10) throw `التقرير قصير جدًا ، ويتألف من 10 أحرف على الأقل!`
     if (text.length > 1000) throw `التقرير طويل جدًا ، بحد أقصى 1000 حرف!`
     let teks = `*${command.toUpperCase()}!*\n\nمن : *@${m.sender.split`@`[0]}*\n\nرسالة : ${text}\n`
