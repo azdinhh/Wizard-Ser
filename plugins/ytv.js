1dc041b01b4bd3dfc1159cf14920311163bb6dd6
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-03 18:34:04 +0000

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -2,13 +2,13 @@ let limit = 30
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw 'uhm... wheres the url?'
+  if (!args || !args[0]) throw 'أوم ... أين عنوان url?'
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   try {
     let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
     let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    m.reply(isLimit ? `File Size: ${filesizeF}\nFile size above ${limit} MB, download it yourself: ${dl_link}` : global.wait)
+    m.reply(isLimit ? `حجم الملف: ${filesizeF}\nلا يمكن تنزيل اكثر من  ${limit} MB, قم بتنزيله بنفسك: ${dl_link}` : global.wait)
     let _thumb = {}
     try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
     catch (e) { }
