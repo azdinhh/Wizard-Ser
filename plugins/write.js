66290486cd48c18af697ef6ee1966a74652cc0d2
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-03 17:33:25 +0000

    Update write.js

--- plugins/write.js
+++ plugins/write.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, args, usedPrefix, command }) => {
     let img = 'https://telegra.ph/file/b29dffd131e4c521c8be.jpg'
-    if (!args[0]) conn.sendFile(m.chat, 'https://telegra.ph/file/fd4230902e75d397d8667.jpg', '', `Please enter destination and filename\nExample: ${ usedPrefix + command } plugins/join.js`, m)
+    if (!args[0]) conn.sendFile(m.chat, 'https://user-images.githubusercontent.com/102433984/161431650-70eabcab-b9b3-4a7e-90b0-7ac30bfccea3.jpg', '', `الرجاء إدخال الوجهة واسم الملف\nمثال: ${ usedPrefix + command } الإضافات/join.js`, m)
     require('fs').writeFileSync(`./${args[0]}`, m.quoted.text)
-    conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), `Successfully save as ${args[0]}`, watermark, 'MENU', '.?', m)
+    conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), `تم الحفظ بنجاح باسم ${args[0]}`, watermark, 'MENU', '.?', m)
 }
 handler.command = /^write$/i
 handler.owner = true
