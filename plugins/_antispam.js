874997b8ac1b0f0a658aaddd5dd2464a3015bff3
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-03 17:02:33 +0000

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -10,7 +10,7 @@ handler.all = async function (m) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 8) {
             if (this.spam[m.sender].count > 10) {
                 db.data.users[m.sender].banned = true
                 await this.sendButton(m.chat, 'لقد تم حظرك بسبب الرسائل غير المرغوب فيها!', watermark, 'Bot owner', ',owner', m)
