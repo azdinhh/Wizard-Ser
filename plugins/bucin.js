fe75f8c8b9ced513c6796572ea3488637c33960d
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-03 18:47:26 +0000

    Update bucin.js

--- plugins/bucin.js
+++ plugins/bucin.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['quotes']
 handler.tags = ['quotes']
 handler.command = /^(quotes)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.premium = true
+handler.group = true
+handler.private = true
 
-handler.admin = false
-handler.botAdmin = false
+handler.admin = true
+handler.botAdmin = true
 
 handler.fail = null
 
