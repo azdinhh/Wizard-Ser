3499ee28ecbc55f1d588326ac9ffef353cc19e15
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-04 00:53:11 +0000

    Update upload.js

--- plugins/upload.js
+++ plugins/upload.js
@@ -4,13 +4,13 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'No media found'
+  if (!mime) throw 'لم يتم العثور على وسائط'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   m.reply(`${link}
 ${media.length} Byte(s)
-${isTele ? '(No Expiration Date)' : '(Not known)'}`)
+${isTele ? '(لا تاريخ انتهاء الصلاحية)' : '(غير معروف)'}`)
 }
 handler.help = ['upload']
 handler.tags = ['tools']
