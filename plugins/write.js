246d97b64db6ddb2539050d44febfc7b5c796d8d
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-04 17:55:51 +0000

    Update write.js

--- plugins/write.js
+++ plugins/write.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, args, usedPrefix, command }) => {
     let img = 'https://telegra.ph/file/b29dffd131e4c521c8be.jpg'
-    if (!args[0]) conn.sendFile(m.chat, 'https://user-images.githubusercontent.com/102433984/161431650-70eabcab-b9b3-4a7e-90b0-7ac30bfccea3.jpg', '', `الرجاء إدخال الوجهة واسم الملف\nمثال: ${ usedPrefix + command } الإضافات/join.js`, m)
+    if (!args[0]) conn.sendFile(m.chat, 'https://user-images.githubusercontent.com/102433984/161603010-d6a695a9-d9a1-4f9e-9661-e15d1e412fa9.gif', '', `الرجاء إدخال الوجهة واسم الملف\nمثال: ${ usedPrefix + command } الإضافات/join.js`, m)
     require('fs').writeFileSync(`./${args[0]}`, m.quoted.text)
     conn.sendButtonImg(m.chat, await(await fetch(img)).buffer(), `تم الحفظ بنجاح باسم ${args[0]}`, watermark, 'MENU', '.?', m)
 }
